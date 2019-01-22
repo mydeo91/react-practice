193ffd61036481e5ff54d53877bff6f1b1cc311e
mydeo91 <leh2030@icloud.com>
2019-01-23 00:09:31 +0900

    django-react 연동 실습

---
+++ Deact/deact-backend/deact/assets/bundles/precache-manifest.49be8a4ad64f7d117685c6e5ceea8451.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "14874b27079a807f8055",
+    "url": "/static/bundles/css/main.0af8d721.chunk.css"
+  },
+  {
+    "revision": "14874b27079a807f8055",
+    "url": "/static/bundles/js/main.14874b27.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/bundles/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "cd71c25a96c5596961f5",
+    "url": "/static/bundles/js/runtime~main.cd71c25a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/bundles/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ae78f6877c5a023cc4db85ee319c7063",
+    "url": "/static/bundles/index.html"
+  }
+];
